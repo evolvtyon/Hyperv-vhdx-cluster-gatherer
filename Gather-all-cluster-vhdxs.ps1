da736ab35b436f22f1776cee576dc4450a84f508
Francisco Ramos <franciscoacramos@gmail.com>
2020-12-15 18:31:50 +0000

    Update Gather-all-cluster-vhdxs.ps1

--- Gather-all-cluster-vhdxs.ps1
+++ Gather-all-cluster-vhdxs.ps1
@@ -15,6 +15,7 @@ Get-ClusterGroup -Cluster $ClusterName | `
                                                   VhdFormat, `
                                                   VhdType, `
                                                   @{label=’Size On Physical Disk (GB)’;expression={$_.FileSize/1gb –as [int]}}, `
-                                                  @{label=’Max Disk Size (GB)’;expression={$_.Size/1gb –as [int]}}, @{label=’Remaining Space (GB)’;expression={($_.Size/1gb - $_.FileSize/1gb) –as [int]}} | `
+                                                  @{label=’Max Disk Size (GB)’;expression={$_.Size/1gb –as [int]}}, `
+                                                  @{label=’Remaining Space (GB)’;expression={($_.Size/1gb - $_.FileSize/1gb) –as [int]}} | `
                                     Export-Csv -Path “C:\$($ClusterName)-vhdsByVm.csv" -NoTypeInformation
 }
