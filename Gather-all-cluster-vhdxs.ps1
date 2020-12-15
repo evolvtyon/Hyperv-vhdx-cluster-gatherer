21c4d953b6840a8c04cf05f06d8b03ce2921ab03
Francisco Ramos <franciscoacramos@gmail.com>
2020-12-15 18:31:11 +0000

    Update Gather-all-cluster-vhdxs.ps1

--- Gather-all-cluster-vhdxs.ps1
+++ Gather-all-cluster-vhdxs.ps1
@@ -5,10 +5,16 @@ Function Gather-all-cluster-vhdxs {
     [string]$ClusterName
     )
 
-Get-ClusterGroup -Cluster $ClusterName | ? {$_.GroupType -eq 'VirtualMachine' } | Get-VM | `
-% { $vhd = Get-VHD -ComputerName $_.ComputerName -VmId $_.VmId; $vhd | Add-Member -NotePropertyName “Name” -NotePropertyValue $_.Name; $vhd; } | `
-Select-Object Name, @{label=’Host Name’;expression={$_.ComputerName}}, Path, VhdFormat, VhdType, @{label=’Size On Physical Disk (GB)’;expression={$_.FileSize/1gb –as [int]}}, `
-@{label=’Max Disk Size (GB)’;expression={$_.Size/1gb –as [int]}}, @{label=’Remaining Space (GB)’;expression={($_.Size/1gb - $_.FileSize/1gb) –as [int]}} | `
-Export-Csv -Path “C:\$($ClusterName)-vhdsByVm.csv" -NoTypeInformation
-
+Get-ClusterGroup -Cluster $ClusterName | `
+                          ? {$_.GroupType -eq 'VirtualMachine' } | Get-VM | `
+                            % { $vhd = Get-VHD -ComputerName $_.ComputerName -VmId $_.VmId; $vhd | `
+                                Add-Member -NotePropertyName “Name” -NotePropertyValue $_.Name; $vhd; } | `
+                                    Select-Object Name, `
+                                                  @{label=’Host Name’;expression={$_.ComputerName}}, `
+                                                  Path, `
+                                                  VhdFormat, `
+                                                  VhdType, `
+                                                  @{label=’Size On Physical Disk (GB)’;expression={$_.FileSize/1gb –as [int]}}, `
+                                                  @{label=’Max Disk Size (GB)’;expression={$_.Size/1gb –as [int]}}, @{label=’Remaining Space (GB)’;expression={($_.Size/1gb - $_.FileSize/1gb) –as [int]}} | `
+                                    Export-Csv -Path “C:\$($ClusterName)-vhdsByVm.csv" -NoTypeInformation
 }
